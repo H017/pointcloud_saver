4142b56ef7b7b379196250971f380127df85dd97
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2014-07-23 13:30:05 -0400

    Cleaned code

--- src/pointcloud_saver_node.cpp
+++ src/pointcloud_saver_node.cpp
@@ -1,22 +1,16 @@
 #include <ros/ros.h>
 #include <std_srvs/Empty.h>
-#include <std_msgs/Header.h>
-#include <sensor_msgs/PointCloud2.h>
 #include <pcl/point_types.h>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 #include <tf/transform_listener.h>
 #include <pcl/io/openni_grabber.h>
 
 typedef pcl::PointCloud<pcl::PointXYZRGBA> PointCloud;
 
-ros::NodeHandle* nh;
 std::string savePath, targetFrame;
 tf::TransformListener* tfListener;
-pcl::Grabber* interface;
 PointCloud::Ptr currentCloud;
 ros::Time currentPointCloudTime;
 
@@ -42,15 +36,15 @@ void cloudCallback (const PointCloud::ConstPtr &cloud)
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "pointcloud_saver");
-    nh = new ros::NodeHandle;
+    ros::NodeHandle nh;
     ros::NodeHandle n("~");
 
     // Params
-    nh->param<std::string>("save_path", savePath, "pointclouds");
-    nh->param<std::string>("target_frame", targetFrame, "/map");
+    nh.param<std::string>("save_path", savePath, "pointclouds");
+    nh.param<std::string>("target_frame", targetFrame, "/map");
         
     // Openni
-    interface = new pcl::OpenNIGrabber();
+    pcl::Grabber* interface = new pcl::OpenNIGrabber();
     boost::function<void (const PointCloud::ConstPtr&)> f = boost::bind (cloudCallback, _1);
     interface->registerCallback (f);
     currentCloud = PointCloud::Ptr(new PointCloud);
