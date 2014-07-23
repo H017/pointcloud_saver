85838ee85f56ba06798be9b6d28f7363930ce104
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2014-07-23 13:08:56 -0400

    Modification pour ne pas utiliser le package openni_camera

--- src/pointcloud_saver_node.cpp
+++ src/pointcloud_saver_node.cpp
@@ -9,43 +9,34 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 #include <tf/transform_listener.h>
+#include <pcl/io/openni_grabber.h>
 
-typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
+typedef pcl::PointCloud<pcl::PointXYZRGBA> PointCloud;
 
 ros::NodeHandle* nh;
 std::string savePath, targetFrame;
 tf::TransformListener* tfListener;
+pcl::Grabber* interface;
+PointCloud::Ptr currentCloud;
+ros::Time currentPointCloudTime;
 
 bool save_pc_callback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
 {
-    sensor_msgs::PointCloud2::ConstPtr pc = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/camera/depth_registered/points", *nh, ros::Duration(10.0));
-
-    if (!pc)
-    {
-        ROS_ERROR("Could not retrieve point cloud");
-        return false;
-    }
-
-    PointCloud cloud;
-    PointCloud cloud2;
-    pcl::fromROSMsg(*pc, cloud);
-    
-    try
-    {
-        tfListener->waitForTransform(targetFrame, (*pc).header.frame_id, (*pc).header.stamp, ros::Duration(5.0));
-        pcl_ros::transformPointCloud(targetFrame, cloud, cloud2, *tfListener);
-    }
-    catch (tf::TransformException ex)
-    {
-        ROS_ERROR("%s",ex.what());
-        return false;
-    }
+    tfListener->waitForTransform(targetFrame, (*currentCloud).header.frame_id, ros::Time(0), ros::Duration(5.0));
+    PointCloud pc;
+    pcl_ros::transformPointCloud(targetFrame, *currentCloud, pc, *tfListener); 
 
     std::stringstream ss;
-    ss << savePath << "/pc_" << (*pc).header.stamp << ".pcd";      
-    pcl::io::savePCDFile (ss.str (), cloud2, true); 
-  
+    ss << savePath << "/pc_" << currentPointCloudTime << ".pcd";      
+    pcl::io::savePCDFile (ss.str (), pc, true); 
+
     return true;
+}
+
+void cloudCallback (const PointCloud::ConstPtr &cloud)
+{
+    currentPointCloudTime = ros::Time::now();
+    pcl::copyPointCloud(*cloud, *currentCloud);
 }
 
 int main(int argc, char **argv)
@@ -53,12 +44,22 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "pointcloud_saver");
     nh = new ros::NodeHandle;
     ros::NodeHandle n("~");
-    tfListener = new tf::TransformListener;
 
+    // Params
     nh->param<std::string>("save_path", savePath, "pointclouds");
     nh->param<std::string>("target_frame", targetFrame, "/map");
+        
+    // Openni
+    interface = new pcl::OpenNIGrabber();
+    boost::function<void (const PointCloud::ConstPtr&)> f = boost::bind (cloudCallback, _1);
+    interface->registerCallback (f);
+    currentCloud = PointCloud::Ptr(new PointCloud);
+    interface->start ();
 
+    // TF + Service
+    tfListener = new tf::TransformListener;
     ros::ServiceServer service = n.advertiseService("save_pointcloud", save_pc_callback);
-
     ros::spin();
+    
+    interface->stop();
 }
